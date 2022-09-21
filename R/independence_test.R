5f1b37503304fd54ff071e408e151ef6eb976bdd
Tomasz Wlodarczyk <tomasz.wlodarczyk@contractors.roche.com>
2022-09-20 23:50:18 -0700

    file replaced

--- R/independence_test.R
+++ R/independence_test.R
@@ -1,151 +1,91 @@
-#' Building igraph directed graph object based on the output of the getRegulon function
-#'
-#' @param regulon An object returned by the getRegulon or addWeights function
-#' @param mode A character specifying whch type of graph will be built. In 'tg' mode
-#' a bipartite graph is built connecting transcription factors directly to target genes
-#' and ignoring information about mediating regulatory elements; in 'pairs' mode
-#' transcription factors are connected to unique target gene-regulatory element pairs;
-#' in tripartite mode the network is build of three type of vertices (nodes):
-#' trascription factors, regulatory elements and target genes; here the path from
-#' target gene to regulatory element always contains a regulatory elements
-#' @param weights A character specifying which variable should be used to assign
-#' weights to edges. If set to 'NA' then unweighted graph is built.
-#' @importFrom igraph graph_from_data_frame
-#'
-
-build_graph <-function(regulon, mode = "tripartite", weights = "corr"){
-    stopifnot(mode %in% c("tg", "re", "tripartite", "pairs"))
-
-    # give names to the peaks and target genes which will be easy to extract
-    regulon$idxATAC <- paste0(as.character(regulon$idxATAC), "_peak")
-    regulon$target <- paste0(regulon$target, "_gene")
-    if (mode == "re")
-        vertex_columns <- c("tf", "idxATAC")
-
-    else if (mode == "pairs")
-        vertex_columns <- c("tf", "idxATAC", "target")
-
-    else
-        vertex_columns <- c("tf", "target")
-
-    graph_data <- regulon[,na.omit(c(vertex_columns, weights))]
-    if (mode =="tripartite"){
-        # add tf-re data
-        colnames(graph_data) <- na.omit(c("from", "to", weights))
-        graph_data_tf_re <- data.frame(from = regulon$tf, to = regulon$idxATAC)
-
-        # weights between tf and re are set to 1 for weighted graph
-        if (!is.na(weights)) graph_data_tf_re[,weights] <- 1
-        graph_data <- rbind(graph_data, graph_data_tf_re)
-        rm(graph_data_tf_re)
-        vertex_columns <- c("from", "to")
+test_independence <- function(regulon, PeakMatrix, GeneExpressionMatrix, cluster = "C5"){
+    cluster_cells <- which(colData(GeneExpressionMatrix)$Clusters == cluster)
+    GeneExpressionMatrix <- assay(GeneExpressionMatrix)[,cluster_cells]
+    PeakMatrix <- assay(PeakMatrix)[,cluster_cells]
+    range_list <- list()
+    i<-0
+    while(i*1000 < nrow(regulon)){
+        range_list[[i+1]] <- c(1000*(i)+1, min(1000*(i+1), nrow(regulon)))
+        i <- i +1
     }
-    if (mode == "pairs"){
-        # create node names corresponding to re-tg pairs
-        graph_data$target <- paste(graph_data$idxATAC, graph_data$target, sep ="@")
-        graph_data <- graph_data[, c("tf", "target", weights)]
-        vertex_columns <- c("tf", "target")
-    }
+    counts <- BiocParallel::bplapply(X = range_list,
+                                     FUN = test_independence_bp,
+                                     regulon,
+                                     PeakMatrix,
+                                     GeneExpressionMatrix,
+                                     cluster_cells,
+                                     cluster,
+                                    BPPARAM = BiocParallel::MulticoreParam(workers=8))
 
-    if (is.na(weights))
-        graph_data <- unique(graph_data)
-    else{
-        colnames(graph_data)[colnames(graph_data) == weights] <- "weight"
-        grouping_factors <- paste(vertex_columns, collapse="+")
-        aggregation_formula <- eval(parse(text=paste0("weight~", grouping_factors)))
-        graph_data <- aggregate(graph_data, aggregation_formula, mean)
-    }
-    epiregulon_graph <- igraph::graph_from_data_frame(graph_data)
-    if (mode == "tripartite"){
-        layer_numb <- rep(1, igraph::vcount(epiregulon_graph))
-        layer_numb[grepl("_gene$", igraph::V(epiregulon_graph)$name)] <- 2
-        layer_numb[grepl("_peak$", igraph::V(epiregulon_graph)$name)] <- 3
-        igraph::V(epiregulon_graph)$layer <- layer_numb
-    }
-    # set 'type' attribute for vertices required by bipartite graphs
-    vertex_type <- rep("transcription factor", igraph::vcount(epiregulon_graph))
-    vertex_type[grepl("_peak$", igraph::V(epiregulon_graph)$name)] <- "regulatory element"
-    vertex_type[grepl("_gene$", igraph::V(epiregulon_graph)$name)] <- "target gene"
-    igraph::V(epiregulon_graph)$type <- vertex_type
-    # transform character constants to numeric values for later use by graphics functions
-    igraph::V(epiregulon_graph)$type.num <- match(igraph::V(epiregulon_graph)$type,
-                                                  c("transcription factor", "peak", "target gene"))
 
-    # restore original names
-    igraph::V(epiregulon_graph)$name <- gsub("_gene|_peak", "", igraph::V(epiregulon_graph)$name)
-    epiregulon_graph
+    counts <- do.call(rbind, counts)
+    counts <- counts[order(counts$i),]
+    counts <- counts[,colnames(counts)!="i"]
+    regulon <- cbind(regulon, counts)
+    regulon$joint_prob_pred <- regulon$tf_re * regulon$tg
+    regulon
 }
 
 
 
-build_difference_graph <- function(graph_obj_1, graph_obj_2, weighted = TRUE){
-    res <- igraph::graph_from_adjacency_matrix(abs(igraph::get.adjacency(graph_obj_1, attr = "weight") -
-                                                igraph::get.adjacency(graph_obj_2, attr = "weight")), weighted = weighted)
-    V(res)$type <- V(graph_obj_1)$type
-    V(res)$type.num <- V(graph_obj_1)$type.num
-    res
-}
-
-
-calculate_centrality_degree <- function(graph){
-    igraph::V(graph)$centrality <- igraph::strength(graph)
-    graph
-}
-
-rank_tfs <- function(graph){
-    data.frame(tf = V(graph)$name[order(V(graph)$centrality, decreasing = TRUE)],
-               rank = 1:vcount(graph),
-               centrality = sort(V(graph)$centrality, decreasing = TRUE))
-}
-
-
-plot_epiregulon_network <-
-    function(
-        graph,
-        layout = "stress",
-        label_size = 3,
-        tfs_to_label = NULL,
-        edge_alpha = 0.02,
-        tf_point_size = 3,
-        re_and_tg_point_size = 1,
-        tf_point_border_size = 0.5,
-        re_and_tg_point_border_size = 0.5,
-        label_alpha = 0.8,
-        label_nudge_x = 0.5,
-        label_nudge_y = 0.5,
-        ...
-    ) {
-        my_layout <- ggraph::create_layout(graph, layout = layout)
-        highlighted <- my_layout[my_layout$label_col  %in% tfs_to_label, ]
-        my_plot <-
-            ggraph::ggraph(graph = my_layout) +
-            ggraph::geom_edge_link(alpha = 0.02) +
-            ggraph::geom_node_point(
-                ggplot2::aes(fill = type),
-                shape = 21,
-                size = re_and_tg_point_size,
-                stroke = re_and_tg_point_border_size
-            ) +
-            ggraph::geom_node_label(
-                ggplot2::aes(label = name),
-                data = highlighted,
-                alpha = label_alpha,
-                nudge_x = label_nudge_x,
-                nudge_y = label_nudge_y,
-                size = label_size
-            ) +
-            ggraph::geom_node_point(
-                ggplot2::aes(fill = type),
-                shape = 21,
-                data = highlighted,
-                size = 3,
-                stroke = tf_point_border_size
-            ) +
-            ggplot2::theme_void() +
-            ggplot2::labs(fill = NULL)
-
-        return(my_plot)
+test_independence_bp <- function(range_vector, regulon, PeakMatrix, GeneExpressionMatrix, cluster_cells, cluster = "C5"){
+    counts <- data.frame()
+    for (i in range_vector[1]:range_vector[2]){
+        tf_active <- GeneExpressionMatrix[regulon[i, "tf"],]>1
+        tg_active <- GeneExpressionMatrix[regulon[i, "idxRNA"], ]>1
+        re_available <- PeakMatrix[regulon[i, "idxATAC"], ] > 0
+        counts <- rbind(counts, data.frame(i = i, tf_re = sum((tf_active+re_available)==2)/length(cluster_cells),
+                   tg = sum(tg_active)/length(cluster_cells),
+                   triple = sum((tf_active+tg_active+re_available)==3)/length(cluster_cells)))
     }
+    counts
+}
 
+
+reprogram_seq_data_joint_prob <- test_independence(reprogram_seq_data, PeakMatrix, GeneExpressionMatrix)
+
+
+
+add_p_val <- function(regulon, n_cells){
+    p_vals<-c()
+    for (i in seq_len(nrow(regulon))){
+        if (regulon$triple[i] == 0){
+            p_vals[length(p_vals)+1] <- 1
+            next
+        }
+        p_vals[length(p_vals)+1] <- 1 - pbinom(n_cells*regulon[i,"triple"]-1, n_cells, regulon[i,"joint_prob_pred"])
+    }
+    regulon$p_val <- p_vals
+    regulon$p_val_adj <- regulon$p_val*n_cells
+    regulon$p_val_adj[regulon$p_val_adj>1] <- 1
+    regulon
+}
+
+n <- length(which(colData(GeneExpressionMatrix)$Clusters=="C5"))
+
+reprogram_seq_data_joint_prob <- add_p_val(reprogram_seq_data_joint_prob, n)
+
+colnames(reprogram_seq_data_joint_prob)[colnames(reprogram_seq_data_joint_prob) == "p_val"] <- paste0("p_val_", "C5")
+colnames(reprogram_seq_data_joint_prob)[colnames(reprogram_seq_data_joint_prob) == "p_val_adj"] <- paste0("p_val_adj_", "C5")
+colnames(reprogram_seq_data_joint_prob)[colnames(reprogram_seq_data_joint_prob) == "triple"] <- paste0("triple_", "C5")
+colnames(reprogram_seq_data_joint_prob)[colnames(reprogram_seq_data_joint_prob) == "joint_prob_pred"] <- paste0("joint_prob_pred_", "C5")
+colnames(reprogram_seq_data_joint_prob)[colnames(reprogram_seq_data_joint_prob) == "tg"] <- paste0("tg_", "C5")
+colnames(reprogram_seq_data_joint_prob)[colnames(reprogram_seq_data_joint_prob) == "tf_re"] <- paste0("tf_re_", "C5")
+
+add_new_cluster <- function(cluster, old_regulon){
+    regulon <- test_independence(old_regulon, PeakMatrix, GeneExpressionMatrix, cluster = cluster)
+    n = length(which(colData(GeneExpressionMatrix)$Clusters==cluster))
+    regulon <- add_p_val(regulon, n)
+    colnames(regulon)[colnames(regulon) == "p_val"] <- paste0("p_val_", cluster)
+    colnames(regulon)[colnames(regulon) == "p_val_adj"] <- paste0("p_val_adj_", cluster)
+    colnames(regulon)[colnames(regulon) == "triple"] <- paste0("triple_", cluster)
+    colnames(regulon)[colnames(regulon) == "joint_prob_pred"] <- paste0("joint_prob_pred_", cluster)
+    colnames(regulon)[colnames(regulon) == "tg"] <- paste0("tg_", cluster)
+    colnames(regulon)[colnames(regulon) == "tf_re"] <- paste0("tf_re_", cluster)
+    regulon
+}
+
+reprogram_seq_data_joint_prob <- add_new_cluster("C1", reprogram_seq_data_joint_prob)
+reprogram_seq_data_joint_prob <- add_new_cluster("C3", reprogram_seq_data_joint_prob)
 
 
