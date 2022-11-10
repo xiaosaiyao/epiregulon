d106f57072080da2a49e6d97b5fe072c5725d374
Tomasz Wlodarczyk <tomasz.wlodarczyk@contractors.roche.com>
2022-11-10 04:40:56 -0800

    tests for chi square option

--- tests/testthat/test-regulonPruning.R
+++ tests/testthat/test-regulonPruning.R
@@ -32,27 +32,82 @@ for(i in 1:4){
 }
 
 
-test_that("pruneRegulon calculates p-values in binomial test correctly", {
+test_that("pruneRegulon calculates p-values with binomial test correctly", {
   regulon.p <- pruneRegulon(regulon = regulon, expMatrix = geneM, peakMatrix = peakM, regulon_cutoff = 2)
   expect_identical(regulon.p$pval_all, pvals)
 })
 
-
-peakM.b <- peakM.b[,1:50]
-geneM.b <- geneM.b[,1:50]
-tf_re.b <- geneM.b[regulon$tf,]*peakM.b[regulon$idxATAC,]
-triplets.b <- tf_re.b*geneM.b[regulon$target,]
-target.b <- geneM.b[regulon$target,]
-triplet_n <- rowSums(triplets.b)
-tf_re_prob <- rowSums(tf_re.b)/50
-target_prob <- rowSums(target.b)/50
 pvals <- c()
 for(i in 1:4){
-  pvals[i]<-binom.test(triplet_n[i], 50, target_prob[i]*tf_re_prob[i])$p.val
+  pvals[i]<-suppressWarnings(chisq.test(c(triplet_n[i], 100-triplet_n[i]),
+                       p =c(target_prob[i]*tf_re_prob[i] ,
+                            1- target_prob[i]*tf_re_prob[i]))$p.val)
 }
 
-test_that("pruneRegulon calculates cluster p-values in binomial test correctly", {
+test_that("pruneRegulon calculates p-values with chi-square test correctly", {
+  regulon.p <- pruneRegulon(regulon = regulon, expMatrix = geneM, peakMatrix = peakM,
+                            regulon_cutoff = 2, test ="chi.sq")
+  expect_identical(regulon.p$pval_all, pvals[is.finite(pvals)])
+})
+
+# calculating results for the cluster 1
+peakM.b_C1 <- peakM.b[,1:50]
+geneM.b_C1 <- geneM.b[,1:50]
+tf_re.b_C1 <- geneM.b_C1[regulon$tf,]*peakM.b_C1[regulon$idxATAC,]
+triplets.b_C1 <- tf_re.b_C1*geneM.b_C1[regulon$target,]
+target.b_C1 <- geneM.b_C1[regulon$target,]
+triplet_n_C1 <- rowSums(triplets.b_C1)
+tf_re_prob_C1 <- rowSums(tf_re.b_C1)/50
+target_prob_C1 <- rowSums(target.b_C1)/50
+pvals_C1 <- c()
+for(i in 1:4){
+  pvals_C1[i]<-binom.test(triplet_n_C1[i], 50, target_prob_C1[i]*tf_re_prob_C1[i])$p.val
+}
+
+# calculating results for the cluster 2
+peakM.b_C2 <- peakM.b[,51:100]
+geneM.b_C2 <- geneM.b[,51:100]
+tf_re.b_C2 <- geneM.b_C2[regulon$tf,]*peakM.b_C2[regulon$idxATAC,]
+triplets.b_C2 <- tf_re.b_C2*geneM.b_C2[regulon$target,]
+target.b_C2 <- geneM.b_C2[regulon$target,]
+triplet_n_C2 <- rowSums(triplets.b_C2)
+tf_re_prob_C2 <- rowSums(tf_re.b_C2)/50
+target_prob_C2 <- rowSums(target.b_C2)/50
+pvals_C2 <- c()
+for(i in 1:4){
+  pvals_C2[i]<-binom.test(triplet_n_C2[i], 50, target_prob_C2[i]*tf_re_prob_C2[i])$p.val
+}
+
+test_that("pruneRegulon calculates cluster p-values with binomial test correctly", {
   regulon.p <- pruneRegulon(regulon = regulon, expMatrix = geneM, peakMatrix = peakM, regulon_cutoff = 2,
                             clusters = rep(c("C1", "C2"), each = 50))
-  expect_identical(regulon.p$pval_C1, pvals)
+  expect_identical(regulon.p$pval_C1, pvals_C1)
+  expect_identical(regulon.p$pval_C2, pvals_C2)
+})
+
+pvals_C1 <- c()
+for(i in 1:4){
+  pvals_C1[i]<-suppressWarnings(chisq.test(c(triplet_n_C1[i], 50-triplet_n_C1[i]),
+                                        p =c(target_prob_C1[i]*tf_re_prob_C1[i] ,
+                                             1- target_prob_C1[i]*tf_re_prob_C1[i]))$p.val)
+}
+
+pvals_C2 <- c()
+for(i in 1:4){
+  pvals_C2[i]<-suppressWarnings(chisq.test(c(triplet_n_C2[i], 50-triplet_n_C2[i]),
+                                           p =c(target_prob_C2[i]*tf_re_prob_C2[i] ,
+                                                1- target_prob_C2[i]*tf_re_prob_C2[i]))$p.val)
+}
+
+# create data frame with
+pvals <- data.frame(C1 = pvals_C1, C2 = pvals_C2)
+
+# remove rows with NaN values
+pvals <- suppressWarnings(pvals[is.finite(apply(pvals,1,function(x) min(x, na.rm = TRUE))),])
+
+test_that("pruneRegulon calculates cluster p-values with chi-square test correctly", {
+  regulon.p <- pruneRegulon(regulon = regulon, expMatrix = geneM, peakMatrix = peakM, regulon_cutoff = 2,
+                            clusters = rep(c("C1", "C2"), each = 50), test = "chi.sq")
+  expect_identical(regulon.p$pval_C1, pvals$C1)
+  expect_identical(regulon.p$pval_C2, pvals$C2)
 })
