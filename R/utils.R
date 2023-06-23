3028d1c5f04e721512eecb20d536c57990a8d814
Xiaosai Yao <yao.xiaosai@gene.com>
2023-06-23 14:03:48 -0700

    perform log transform

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,12 @@
 #' Convert ArchR Gene Expression Matrix to SingleCellExperiment
 #'
 #' @param rse a RangedSummarizedExperiment object obtained from `ArchR::getMatrixFromProject`
+#' @param rename String indicating the name of the assay to rename
+#' @param transform A logical indicating whether counts should be transformed or normalized.
+#' See @details
+#' @param transform_method String indicating counts should be log2 transformed or
+#' library size normalized then log2 transformed.See @details
+#' @param log_name String indicating the assay name for the log transformed values
 #'
 #' @return A SingleCellExperiment object
 #' @export
@@ -10,28 +16,52 @@
 #' converts the class of the object from `RangedSummarizedExperiment` to `SingleCellExperiment`.
 #' During the conversion, the genomic location of the genes are transferred from `rowData` in the
 #' `RangedSummarizedExperiment` to the `rowRanges` of the `SingleCellExperiment`.
+#' The counts from ArchR project are library size normalized counts. This function allows the user to add
+#' an extra assay to store log transformed counts.
 #'
-ArchRMatrix2SCE <- function(rse) {
+ArchRMatrix2SCE <- function(rse,
+                            rename="counts",
+                            transform=FALSE,
+                            transform_method=c("log","normalize_log"),
+                            log_name="logcounts") {
 
   rowData_temp <- rowData(rse)
   rse <- as(rse, "SingleCellExperiment")
-  rowData_temp$strand[rowData_temp$strand == 1] = "+"
-  rowData_temp$strand[rowData_temp$strand == 2] = "-"
-  rowData_temp$strand[rowData_temp$strand == 3] = "*"
+  names(assays(rse))[1] <- rename
 
-  # change order
-  swap_rows <- which(rowData_temp$start > rowData_temp$end)
-  rowData_temp_start <- rowData_temp$start
-  rowData_temp$start[swap_rows] <- rowData_temp$end[swap_rows]
-  rowData_temp$end[swap_rows] <- rowData_temp_start[swap_rows]
+  if (all(c("strand","start","end", "seqnames") %in% colnames(rowData_temp))){
+    rowData_temp$strand[rowData_temp$strand == 1] = "+"
+    rowData_temp$strand[rowData_temp$strand == 2] = "-"
+    rowData_temp$strand[rowData_temp$strand == 3] = "*"
 
-  rowRanges(rse) <- makeGRangesFromDataFrame(rowData_temp)
-  rowData(rse) <- rowData_temp[, -which(colnames(rowData_temp) %in% c("seqnames","start","end","strand"))]
-  count_names <- switch(names(assays(rse)),
-                        GeneExpressionMatrix = "logcounts",
-                        GeneIntegrationMatrix = "logcounts",
-                        PeakMatrix = "counts")
-  names(assays(rse))[1] <- count_names
+    # change order
+    swap_rows <- which(rowData_temp$start > rowData_temp$end)
+    rowData_temp_start <- rowData_temp$start
+    rowData_temp$start[swap_rows] <- rowData_temp$end[swap_rows]
+    rowData_temp$end[swap_rows] <- rowData_temp_start[swap_rows]
+
+    rowRanges(rse) <- makeGRangesFromDataFrame(rowData_temp)
+    rowData(rse) <- rowData_temp[, -which(colnames(rowData_temp) %in%
+                                            c("seqnames","start","end","strand"))]
+  }
+
+
+
+
+  transform_method <- match.arg(transform_method)
+
+  if (transform) {
+    if (transform_method == "log"){
+      mat <- assay(rse)
+      mat@x <- log2(mat@x+1)
+      assay(rse, log_name) <- mat
+    } else if (transform_method == "normalize_log"){
+      mat <- assay(rse)
+      normalized <- normalizeCols(mat = mat, scaleTo = 10000)
+      normalized@x <- log2(normalized@x+1)
+      assay(rse, log_name) <- normalized
+    }
+  }
   rse
 }
 
@@ -119,4 +149,14 @@ plotDiagnostic <- function(idx,regulon, expMatrix,exp_assay, exp_cutoff=1, peakM
 
 binarize <- function(input_vector, cutoff) {
   filtered <- as.numeric(input_vector > cutoff)
+}
+
+normalizeCols <- function(mat = NULL, scaleTo = NULL){
+  colSm <- Matrix::colSums(mat)
+  if(!is.null(scaleTo)){
+    mat@x <- scaleTo * mat@x / rep.int(colSm, Matrix::diff(mat@p))
+  }else{
+    mat@x <- mat@x / rep.int(colSm, Matrix::diff(mat@p))
+  }
+  return(mat)
 }
