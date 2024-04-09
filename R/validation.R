663545edf32c5e8f998ea560b7fad83bccb7aa04
TomVuod <tomwlo@gmail.com>
2024-04-09 11:05:53 -0700

    changes after BioC review restored

---
+++ R/validation.R
@@ -0,0 +1,28 @@
+.validate_clusters <- function(clusters, expMatrix){
+  clusters <- tryCatch(as.vector(clusters), error = function(cond) {
+    message("'clusters' argument should be coercible to a vector")
+    stop(cond)
+  })
+  if (length(clusters) != ncol(expMatrix)) {
+    stop("'clusters' length should be equal to the number of cells")
+  }
+  if (any(is.na(clusters))) {
+    stop("'clusters' object contains NA")
+  }
+}
+
+.balance_check <- function(peak_cutoff, exp_cutoff, peakMatrix, expMatrix){
+  if (!is.null(peak_cutoff)) {
+    prop <- sum(peakMatrix > peak_cutoff)/prod(dim(peakMatrix))
+    if (prop < 1e-04 | prop > 0.9999)
+      warning(sprintf("Strong inbalance between groups after applying cutoff to peakMatrix. Consider %s value of the peak_cutoff",
+                      c("increasing", "decreasing")[(prop < 1e-04) + 1]))
+  }
+  if (!is.null(exp_cutoff)) {
+    prop <- sum(expMatrix > exp_cutoff)/prod(dim(expMatrix))
+    if (prop < 1e-04 | prop > 0.9999)
+      warning(sprintf("Strong inbalance between groups after applying cutoff to expMatrix. Consider %s value of the exp_cutoff",
+                      c("increasing", "decreasing")[(prop < 1e-04) + 1]))
+  }
+}
+
