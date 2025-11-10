b3102ca1e18c0e505091c4f339e1a705b7689331
TomVuod <tomasz.wlodarczyk@contractors.roche.com>
2025-11-10 11:36:32 +0000

    message formatting

--- R/calculateP2G.R
+++ R/calculateP2G.R
@@ -135,7 +135,7 @@ calculateP2G <- function(peakMatrix = NULL,
     stop("rowData of expMatrix does not contain ", gene_symbol)
   }
   if(!is(cellNum, "CellNumSol") && as.list(sys.call(sys.nframe()-1))[[1]]!="optimizeMetacellNumber"){
-      message(strwrap("Value of the paramater 'cellNum' has not been optimized.
+    message(strwrap("Value of the paramater 'cellNum' has not been optimized.
                 Consider running function 'optimizeMetacellNumber' and use output to set 'cellNum'."))
   }
   if(is(cellNum,"CellNumSol")){
